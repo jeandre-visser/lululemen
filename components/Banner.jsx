1bb937dfeb78751ccdea040ee14653ef17618a4b
Jeandre Visser <jeandrev1414@gmail.com>
2022-11-16 15:57:30 -0700

    Build banner

--- components/Banner.jsx
+++ components/Banner.jsx
@@ -1,8 +1,24 @@
-import React from 'react'
+import React from 'react';
+import Link from 'next/link';
 
 const Banner = () => {
   return (
-    <div>Banner</div>
+    <div className="banner-container">
+      <div>
+        <p className="metal-vent">TEXT</p>
+        <h3>MID</h3>
+        <img src="" alt="metal-vent-shirt" className="banner-image" />
+        <div>
+          <Link href="/product/id" >
+            <button type="button" >BUTTON</button>
+          </Link>
+          <div className="desc">
+            <h5>DESC</h5>
+            <p>description</p>
+          </div>
+        </div>
+      </div>
+    </div>
   )
 }
 
