3d4056df6a8155e16cd4a3663a6e766ca7d30c0c
Jeandre Visser <jeandrev1414@gmail.com>
2022-11-17 16:49:59 -0700

    Create a footnote and style

--- components/BannerFooter.jsx
+++ components/BannerFooter.jsx
@@ -1,14 +1,22 @@
 import React from 'react';
 import { images } from '../constants/index';
 import Image from 'next/image';
+import { RiCopyrightFill } from 'react-icons/ri';
 
 const BannerFooter = () => {
   return (
-    <div className="banner-footer-container">
-      <h1>Push Your Limits, with Limitless Apparal</h1>
-      <Image src={images.footer} alt='footer' />
-    </div>
-  )
+    <>
+      <div className="banner-footer-container">
+        <h1>Push Your Limits, with Limitless Apparal</h1>
+        <Image src={images.footer} alt='footer' />
+      </div>
+      <div className='footnote'>
+        <Image src={images.footnote} alt='logo' className="footer-logo" />
+        <p><RiCopyrightFill fontSize={18} /> 2022 Lululemen <br/>All Rights Reserved</p>
+      </div>
+    </>
+    )
+    
 }
 
 export default BannerFooter
