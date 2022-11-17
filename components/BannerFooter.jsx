a7de733078af2e7ddc2419891a7cd2cd930106d9
Jeandre Visser <jeandrev1414@gmail.com>
2022-11-17 16:27:03 -0700

    Build banner

--- components/BannerFooter.jsx
+++ components/BannerFooter.jsx
@@ -1,8 +1,13 @@
-import React from 'react'
+import React from 'react';
+import { images } from '../constants/index';
+import Image from 'next/image';
 
 const BannerFooter = () => {
   return (
-    <div>BannerFooter</div>
+    <div className="banner-footer-container">
+      <h1>Push Your Limits, with Limitless Apparal</h1>
+      <Image src={images.footer} alt='footer' />
+    </div>
   )
 }
 
