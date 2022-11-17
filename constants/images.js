afc8aa001767ce0f37b6836c37d27c75b871ae8f
Jeandre Visser <jeandrev1414@gmail.com>
2022-11-17 16:51:29 -0700

    Export images

--- constants/images.js
+++ constants/images.js
@@ -1,6 +1,11 @@
-import logo from '../assets/logo2.png';
+import logo from '../assets/logo.png';
+import icon from '../assets/icon.png';
+import footer from '../assets/footer.png';
+import footnote from '../assets/footnote.png';
 
 export default {
   logo,
-  
+  icon,
+  footer,
+  footnote,
 }
