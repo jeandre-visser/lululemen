5c9486e074b014ed178a2baec284ae58402d9daa
Jeandre Visser <jeandrev1414@gmail.com>
2022-11-16 18:51:57 -0700

    Build product card

--- components/Product.jsx
+++ components/Product.jsx
@@ -1,8 +1,24 @@
-import React from 'react'
+import React from 'react';
+import Link from 'next/link';
+import { urlFor } from '../lib/client';
+import Image from 'next/image';
 
-const Product = () => {
+const Product = ({ product: { image, name, slug, price } }) => {
+
   return (
-    <div>Product</div>
+    <div>
+      <Link href={`/product/${slug.current}`}>
+        <div className="product-card" >
+          <Image
+            className="product-image"
+            src={urlFor(image && image[0])}
+            layout="responsive"
+          />
+          <p className='product-name'>{name}</p>
+          <p className='product-price'>${price}</p>
+        </div>
+      </Link>
+    </div>
   )
 }
 
