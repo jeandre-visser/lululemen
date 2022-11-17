eec21f1cbac18c9684c0c1126b3577ac0c89d478
Jeandre Visser <jeandrev1414@gmail.com>
2022-11-17 15:12:41 -0700

    Import all style files

--- pages/_app.js
+++ pages/_app.js
@@ -1,4 +1,11 @@
 import '../styles/globals.css'
+import '../styles/Product.scss'
+import '../styles/Banner.scss'
+import '../styles/BannerFooter.scss'
+import '../styles/Layout.scss'
+import '../styles/Cart.scss'
+import '../styles/Navbar.scss'
+import '../styles/Footer.scss'
 
 function MyApp({ Component, pageProps }) {
   return <Component {...pageProps} />
