0771b7a29245a4889c5acfd293a1e4653105a251
Jeandre Visser <jeandrev1414@gmail.com>
2022-11-17 17:05:56 -0700

    Add social media icons and style

--- components/BannerFooter.jsx
+++ components/BannerFooter.jsx
@@ -2,13 +2,19 @@ import React from 'react';
 import { images } from '../constants/index';
 import Image from 'next/image';
 import { RiCopyrightFill } from 'react-icons/ri';
+import { BsInstagram, BsTwitter } from 'react-icons/bs';
 
 const BannerFooter = () => {
   return (
     <>
       <div className="banner-footer-container">
-        <h1>Push Your Limits, with Limitless Apparal</h1>
+        <h1>Push Your Limits, with Limitless Apparel</h1>
         <Image src={images.footer} alt='footer' />
+      <div className="socials">
+        <h2>Follow Us On: </h2> 
+        <BsInstagram fontSize={30} />
+        <BsTwitter fontSize={30} />
+      </div>
       </div>
       <div className='footnote'>
         <Image src={images.footnote} alt='logo' className="footer-logo" />
