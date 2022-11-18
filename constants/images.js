8763551a93731be078e7506063bf07fa2e8114a5
Jeandre Visser <jeandrev1414@gmail.com>
2022-11-17 17:24:51 -0700

    Lulu logo

--- constants/images.js
+++ constants/images.js
@@ -2,10 +2,12 @@ import logo from '../assets/logo.png';
 import icon from '../assets/icon.png';
 import footer from '../assets/footer.png';
 import footnote from '../assets/footnote.png';
+import lulu from '../assets/lulu.png';
 
 export default {
   logo,
   icon,
   footer,
   footnote,
+  lulu,
 }
